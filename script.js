f607fed2dc7bd4deb5340a676181f03ea10f95b0
Amna Shabbir <122596364+AmnaShabbir31@users.noreply.github.com>
2023-01-21 11:24:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
+
+function toggleButton(){
+  navList.classList.toggle("show");
+}
+
+hamburger.addEventListener("click", toggleButton);
